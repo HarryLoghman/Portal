1ba5188b4b6f50227420dd380ca551ced314e304
Behrooz <Behrooz@DESKTOP-989A5MH>
2016-12-04 12:53:01 +0330

    commit

--- SharedLibrary/Date.cs
+++ SharedLibrary/Date.cs
@@ -37,14 +37,14 @@ namespace SharedLibrary
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
             System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            dtDateTime = dtDateTime.AddMilliseconds(unixTimeStamp);
             return dtDateTime;
         }
 
         public static double DateTimeToUnixTimestamp(DateTime dateTime)
         {
             return (TimeZoneInfo.ConvertTimeToUtc(dateTime) -
-                   new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds;
+                   new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalMilliseconds;
         }
     }
 }
