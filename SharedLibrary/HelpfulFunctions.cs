e57243485c0b95d5fbdb21126e3eb89e530db7b7
behrooz_sohrabloo <b.sohrabloo@gmail.com>
2017-09-11 22:39:38 +0430

    commit

--- SharedLibrary/HelpfulFunctions.cs
+++ SharedLibrary/HelpfulFunctions.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using ICSharpCode.SharpZipLib.BZip2;
+using Newtonsoft.Json;
+using SharedLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SharedLibrary
 {
     public class HelpfulFunctions
     {
+        static log4net.ILog logs = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static string GetAllTheNumbersFromComplexString(string input)
         {
             return new String(input.Where(Char.IsDigit).ToArray());
@@ -22,5 +29,84 @@ namespace SharedLibrary
             return dynamicObject.GetType().GetProperty(propertyName) != null;
         }
 
+        public static bool DownloadFileFromWeb(string uri, string filePath)
+        {
+            var fileName = uri.Remove(0, uri.LastIndexOf('/'));
+            try
+            {
+                WebClient Client = new WebClient();
+                Client.DownloadFile(uri, filePath + fileName);
+                return true;
+            }
+            catch (Exception e)
+            {
+                logs.Error("Exception in DownloadFileFromWeb-" + uri + ":", e);
+                return false;
+            }
+        }
+
+        private static bool DeleteFile(string fileUri)
+        {
+            try
+            {
+                File.Delete(fileUri);
+                return true;
+            }
+            catch (Exception e)
+            {
+                logs.Error("Exception in DeleteFile-" + fileUri + ":", e);
+                return false;
+            }
+        }
+
+        public static bool DecompressFromBZ2File(string fileUri)
+        {
+            FileInfo zipFile = new FileInfo(fileUri);
+            using (FileStream fileToDecompressAsStream = zipFile.OpenRead())
+            {
+                string decompressedFileName = fileUri.Replace(".bz2", "");
+                using (FileStream decompressedStream = File.Create(decompressedFileName))
+                {
+                    try
+                    {
+                        BZip2.Decompress(fileToDecompressAsStream, decompressedStream, true);
+                        return true;
+                    }
+                    catch (Exception e)
+                    {
+                        logs.Error("Exception in DecompressFromBZ2File-" + fileUri + ":", e);
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public static List<ImiData> ReadImiDataFile(string fileUri)
+        {
+            var preparedLine = "";
+            var imiDataList = new List<ImiData>();
+            try
+            {
+                var file = File.ReadAllLines(fileUri);
+                foreach (var line in file)
+                {
+                    preparedLine = "";
+                    preparedLine = line.Replace("trans-id", "transId");
+                    preparedLine = preparedLine.Replace("base-price-point", "basePricePoint");
+                    preparedLine = preparedLine.Replace("next_renewal_date", "nextRenewalDate");
+                    preparedLine = preparedLine.Replace("trans-status", "transStatus");
+                    preparedLine = preparedLine.Replace("billed-price-point", "billedPricePoint");
+                    preparedLine = preparedLine.Replace("event-type", "eventType");
+                    var imiDataObj = JsonConvert.DeserializeObject<ImiData>(preparedLine);
+                    imiDataObj.keyword = HttpUtility.UrlDecode(imiDataObj.keyword, System.Text.UnicodeEncoding.Default);
+                    imiDataList.Add(imiDataObj);
+                }
+            }
+            catch (Exception e)
+            {
+                logs.Error("Exception in ReadImiDataFile-" + fileUri + ":", e);
+            }
+            return imiDataList;
+        }
     }
 }
---
+++ SharedLibrary/Models/ImiData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Models
+{
+    public class ImiData
+    {
+        public int sid { get; set; }
+        public string transId { get; set; }
+        public int status { get; set; }
+        public int? basePricePoint { get; set; }
+        public string msisdn { get; set; }
+        public string keyword { get; set; }
+        public int validity { get; set; }
+        public DateTime? next_renewal_date { get; set; }
+        public string shortcode { get; set; }
+        public int? billedPricePoint { get; set; }
+        public int transStatus { get; set; }
+        public string chargeCode { get; set; }
+        public DateTime datetime { get; set; }
+        public string eventType { get; set; }
+        public string Channel { get; set; }
+    }
+}
