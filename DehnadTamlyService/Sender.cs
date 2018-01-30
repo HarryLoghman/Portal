ef27c0feca959706544bbee9d67dded4bbe00be0
behrooz_sohrabloo <b.sohrabloo@gmail.com>
2018-01-31 01:08:33 +0330

    commit

--- DehnadTamlyService/Sender.cs
+++ DehnadTamlyService/Sender.cs
@@ -47,7 +47,7 @@ namespace DehnadTamlyService
                 //}
 
                 SharedLibrary.MessageHandler.SendSelectedMessages(entityType, onDemandMessages, skip, take, serviceAdditionalInfo, aggregatorName);
-                if (DateTime.Now.Hour <= 20 && DateTime.Now.Hour > 8)
+                if (DateTime.Now.Hour <= 20 && DateTime.Now.Hour >= 8)
                 {
                     autochargeMessages = ((IEnumerable)SharedLibrary.MessageHandler.GetUnprocessedMessages(entityType, SharedLibrary.MessageHandler.MessageType.AutoCharge, readSize)).OfType<AutochargeMessagesBuffer>().ToList();
                     eventbaseMessages = ((IEnumerable)SharedLibrary.MessageHandler.GetUnprocessedMessages(entityType, SharedLibrary.MessageHandler.MessageType.EventBase, readSize)).OfType<EventbaseMessagesBuffer>().ToList();
