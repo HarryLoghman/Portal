fe8088b6e9447b390842c6f54844fd9588c4f873
behrooz_sohrabloo <b.sohrabloo@gmail.com>
2018-03-26 18:19:25 +0430

    commit

--- DehnadPhantomService/Statistic.cs
+++ DehnadPhantomService/Statistic.cs
@@ -14,7 +14,7 @@ namespace DehnadPhantomService
         {
             try
             {
-                AutochargeStatistic();
+                //AutochargeStatistic();
                 EventbaseStatistic();
             }
             catch (Exception e)
