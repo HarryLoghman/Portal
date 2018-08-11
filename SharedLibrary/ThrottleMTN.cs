ccecbe820b192c57356bbab8bf7c3f8e345a275d
HADI <HadiLoghman@bitbucket.org>
2018-08-11 13:27:18 +0430

    e

--- SharedLibrary/ThrottleMTN.cs
+++ SharedLibrary/ThrottleMTN.cs
@@ -69,6 +69,7 @@ namespace SharedLibrary
         public void throttleRequests(string serviceName, string mobileNumber, string guid)
         {
 
+
             if (!File.Exists(this.v_mapFilePath))
             {
                 throw new Exception(this.v_mapFilePath + " does not exists");
